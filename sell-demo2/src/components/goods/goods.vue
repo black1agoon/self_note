b46e4f9c4b13a53c10c1b73a01f41ea13ff1546c
569078987 <569078987@qq.com>
2017-12-09 15:15:25 +0800

    sell-demo2

---
+++ sell-demo2/src/components/goods/goods.vue
@@ -0,0 +1,26 @@
+<template>
+	<div class="goods">
+		<div class="menu-wrapper"></div>
+		<div class="foods-wrapper"></div>
+	</div>
+</template>
+<script type="text/ecmascript-6">
+	export default {
+	
+	};
+</script>
+<style lang="stylus" rel="stylesheet/stylus">
+	.goods
+		position:absolute
+		display:flex
+		top:174px
+		width:100%
+		bottom:46px
+		overflow:hidden
+		.menu-wrapper
+			flex:0 0 80px
+			width:80px
+			background:#f3f5f7
+		.foods-wrapper
+			flex:1
+</style>
---
+++ sell-demo2/src/main.js
@@ -0,0 +1,38 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+import VueResource from 'vue-resource';
+import App from './App';
+import goods from 'components/goods/goods';
+import ratings from 'components/ratings/ratings';
+import seller from 'components/seller/seller';
+
+import 'common/stylus/index.styl';
+
+Vue.config.productionTip = false;
+Vue.use(VueRouter);
+Vue.use(VueResource);
+// let app=Vue.extend(App);
+const routes = [
+	{path: '/', redirect: '/goods'},
+	{path: '/goods', component: goods},
+	{path: '/ratings', component: ratings},
+	{path: '/seller', component: seller}
+];
+
+const router = new VueRouter({
+  routes,
+  linkActiveClass: 'active'
+
+});
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  template: '<App/>',
+  components: {
+    App
+  }
+});
+
